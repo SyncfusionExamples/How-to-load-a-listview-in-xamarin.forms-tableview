39edc4faa52ca6295cc7ba282b5ea4b99fb8f2bb
Gnana Priya <gnanapriya.namasivayam@syncfusion.com>
2018-12-12 10:49:42 +0530

    Sample and readme file committed to load listview in table view.

---
+++ ListViewInTableView/ListViewInTableView/ListViewInTableView/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Syncfusion.DataSource;
+using Syncfusion.ListView.XForms;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SfListViewSample
+{
+    public partial class MainPage : ContentPage
+    {
+        ContactsViewModel viewmodel;
+        #region Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            listView.DataSource.GroupDescriptors.Add(new GroupDescriptor()
+            {
+                PropertyName = "ContactName",
+                KeySelector = (object obj1) =>
+                {
+                    var item = (obj1 as Contacts);
+                    return item.ContactName[0].ToString();
+                },
+            });
+            viewmodel = new ContactsViewModel();
+            listView.BindingContext = viewmodel;
+        }
+        
+        #endregion
+    }
+   
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# How-to-load-a-listview-in-xamarin.forms-tableview
-This example demonstrates to load a listview in xamarin.forms tableview.
+# How to load a listview in Xamarin.Forms TableView?
+
+This example demonstrates how to load a listview in Xamarin.Forms TableView.
+
+## Requirements to run the demo
+
+* [Visual Studio 2017](https://visualstudio.microsoft.com/downloads/) or [Visual Studio for Mac](https://visualstudio.microsoft.com/vs/mac/)
+* Xamarin add-ons for Visual Studio (available via the Visual Studio installer).
+
+## Troubleshooting
+
+### Path too long exception
+
+If you are facing path too long exception when building this example project, close Visual Studio and rename the repository to short and build the project.
